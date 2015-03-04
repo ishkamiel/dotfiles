19526e1f1dd0b4524d5887b7cfc91b313d5a503d
Hans Liljestrand <ishkamiel@gmail.com>
2015-03-05 01:28:13 +0200

    addex xbacklight

--- scripts/install_i3.sh
+++ scripts/install_i3.sh
@@ -4,7 +4,7 @@
 ASK=
 PACKAGE_DESCRIPTTION="i3 window manager"
 
-PACKAGES=( feh i3 yeahconsole roxterm compton lightdm-gtk-greeter keychain
+PACKAGES=( feh i3 yeahconsole roxterm compton lightdm-gtk-greeter keychain lxappearance gpicview
 lightdm xautolock wicd-gtk )
 
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib/install_packages.sh
