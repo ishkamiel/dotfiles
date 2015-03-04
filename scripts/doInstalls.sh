2efd0e5bad4926daba5e2c76d55af6a48bda1a71
Hans Liljestrand <ishkamiel@gmail.com>
2015-03-05 00:10:16 +0200

    smarter aptitude installing

---
+++ scripts/doInstalls.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+COLOR_RES='\033[0m'
+COLOR_DO='\033[0;31m'
+COLOR_INFO='\033[0;32m'
+COLOR_ASK='\033[0;33m'
+
+INSTALL_HISTORY=~/.pdDotfileAptH
+INSTALL_SCHEDULE=~/.pdDotfileAptS
+
+function say_do {
+    echo -en "${COLOR_DO}$1${COLOR_RES}\n"
+}
+
+function say_info {
+    echo -e "${COLOR_INFO}$1${COLOR_RES}"
+}
+
+if [ -e ${INSTALL_SCHEDULE} ]; then
+    #PACKAGES=$(cat ${INSTALL_SCHEDULE} | tr "\\n" " ")
+    IFS=" " read -a PACKAGES <<< $(cat ${INSTALL_SCHEDULE} | sort | tr "\\n" " ")
+    rm ${INSTALL_SCHEDULE}
+
+    if [ ${#PACKAGES[@]} -eq 0 ]; then
+        say_info "Nothing to install..."
+    else
+        say_do "Running aptitude to install: ${PACKAGES[*]}"
+
+        if sudo aptitude install ${PACKAGES[*]}; then 
+            for var in "${PACKAGES[@]}"
+            do
+                printf "%s\n" "${var}" >> "${INSTALL_HISTORY}"
+            done
+        else
+            say_do "INSTALL FAILED!"
+            exit 1
+        fi
+    fi
+fi
--- scripts/lib/install_packages.sh
+++ scripts/lib/install_packages.sh
@@ -3,6 +3,9 @@ COLOR_DO='\033[0;31m'
 COLOR_INFO='\033[0;32m'
 COLOR_ASK='\033[0;33m'
 
+INSTALL_HISTORY=~/.pdDotfileAptH
+INSTALL_SCHEDULE=~/.pdDotfileAptS
+
 function say_do {
     echo -en "${COLOR_DO}$1${COLOR_RES}\n"
 }
@@ -21,23 +24,46 @@ function say_ask
     eval $__retval="'$retval'"
 }
 
-if [ ${ASK} ]; then 
-    say_info "Installing stuff for ${PACKAGE_DESCRIPTTION}:\n${PACKAGES[*]}\nAre you sure?"
-    say_ask yn ""
+IPACKAGES=()
+
+for var in "${PACKAGES[@]}"
+do
+    if [ ! -f ${INSTALL_HISTORY} ]; then
+        IPACKAGES+=(${var})
+    elif [ $(grep -c ^${var}$ ${INSTALL_HISTORY}) -eq 0 ]; then
+        IPACKAGES+=(${var})
+    fi
+done
+
+if [ ${#IPACKAGES[@]} -eq 0 ]; then
+    say_info "Skipping, already installed..."
 else
-    yn='y'
+
+    if [ ${ASK} ]; then 
+        say_info "Installing stuff for ${PACKAGE_DESCRIPTTION}:\n${IPACKAGES[*]}\nAre you sure?"
+        say_ask yn ""
+    else
+        yn='y'
+    fi
+
+    case $yn in
+        [Yy]*) 
+            if [ ${ASK} ]; then 
+                say_do "Okay, scheduling aptitude installations..."
+            else
+                say_do "Scheduling aptitude installation of: ${IPACKAGES[*]}"
+            fi
+            # sudo aptitude install -y --schedule-only ${IPACKAGES[*]}
+
+            for var in "${IPACKAGES[@]}"
+            do
+                printf "%s\n" "${var}" >> "${INSTALL_SCHEDULE}"
+            done
+            ;;
+        [Nn]*) 
+            #say_info "Okay, skipping installation..."
+            ;;
+    esac
 fi
 
-case $yn in
-    [Yy]*) 
-        if [ ${ASK} ]; then 
-            say_do "Okay, scheduling aptitude installations..."
-        else
-            say_do "Scheduling aptitude installation of: ${PACKAGES[*]}"
-        fi
-        sudo aptitude install -y --schedule-only ${PACKAGES[*]}
-        ;;
-    [Nn]*) 
-        #say_info "Okay, skipping installation..."
-        ;;
-esac
+echo "and here we are"
