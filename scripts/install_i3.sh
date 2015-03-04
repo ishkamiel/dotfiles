aed410bd8ba76f4cc7fe9d6315ea812ec01c6231
Hans Liljestrand <ishkamiel@gmail.com>
2015-03-05 00:46:12 +0200

    added lightdm

--- scripts/install_i3.sh
+++ scripts/install_i3.sh
@@ -3,7 +3,8 @@
 # leave this empty to disable any asking
 ASK=
 PACKAGE_DESCRIPTTION="i3 window manager"
+
 PACKAGES=( feh i3 yeahconsole roxterm compton lightdm-gtk-greeter keychain
-xautolock wicd-gtk )
+lightdm xautolock wicd-gtk )
 
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib/install_packages.sh
