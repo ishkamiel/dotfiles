da12625b9e82f7b580c5aea8a22ab4d16a9722c7
Hans Liljestrand <ishkamiel@gmail.com>
2015-03-13 13:45:21 +0200

    back to wicd

--- scripts/install_i3.sh
+++ scripts/install_i3.sh
@@ -5,6 +5,8 @@ ASK=
 PACKAGE_DESCRIPTTION="i3 window manager"
 
 PACKAGES=( feh i3 guake compton gdm lxappearance xautolock
-network-manager-gnome gnome-keyring gnome-terminal )
+gnome-keyring gnome-terminal 
+wicd-gtk
+)
 
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib/install_packages.sh
