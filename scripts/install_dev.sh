74a96ba128c3d52c14039e8739e05d5c2cd04cbc
Hans Liljestrand <ishkamiel@gmail.com>
2015-03-30 09:36:32 +0300

    added dev stuff

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -6,6 +6,6 @@ ASK=
 PACKAGE_DESCRIPTTION="various development stuff"
 
 PACKAGES=(build-essential doxygen libcppunit-dev cmake ninja-build python-dev
-exuberant-ctags libsdl2-dev)
+exuberant-ctags libsdl2-dev libglew-dev)
 
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib/install_packages.sh
