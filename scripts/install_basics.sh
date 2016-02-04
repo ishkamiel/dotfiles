43729cfbf97ea83b19997a9b801eba76ba766459
Hans Liljestrand <ishkamiel@gmail.com>
2016-02-04 19:26:00 +0200

    major cleanup

--- scripts/install_basics.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# leave this empty to disable any asking
-ASK=
-PACKAGE_DESCRIPTTION="i3 window manager"
-
-PACKAGES=(vim-nox git imagemagick)
-
-source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/lib/install_packages.sh
