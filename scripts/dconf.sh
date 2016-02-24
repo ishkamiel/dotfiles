11d3101dadd4c38f960abd2ad3b61fd9f2f61750
Hans Liljestrand <ishkamiel@gmail.com>
2016-02-24 13:27:10 +0200

    disable type on search, dconf->gsettings

--- scripts/dconf.sh
+++ scripts/dconf.sh
@@ -2,7 +2,10 @@
 
 # Fix desktop background and right-click
 gsettings set org.gnome.settings-daemon.plugins.background active true
+# Disable annoying search on type thing
+gsettings set org.gnome.nautilus.preferences enable-interactive-search false
+# Enable delete contect menu action
+gsettings set org.gnome.nautilus.preferences enable-delete true
 
 # Caps lock to escape
-dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
-
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
