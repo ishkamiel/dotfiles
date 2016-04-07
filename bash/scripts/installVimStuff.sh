bcad2db708f2cbf18c5f12aec356f9969a573028
Hans Liljestrand <ishkamiel@gmail.com>
2016-04-07 13:04:07 +0300

    vim plugin install still buggy

--- bash/scripts/installVimStuff.sh
+++ bash/scripts/installVimStuff.sh
@@ -10,8 +10,10 @@ URL_PLUG="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 ##########################
 
 if [[ ! -e "${FN_VIM}" ]]; then
+    mkdir -p ${FN_VIM}
 	downloadFile "${URL_PLUG}" "${FN_VIM}"
 fi
+echo "Trying to launch vim for plugin install"
 [[ -e ${FN_VIM} ]] && vim +PlugInstall +qall
 
 # Install vim-plug for neovim IF it's installed
@@ -21,7 +23,9 @@ if command -v nvim >/dev/null 2>&1
 then
 	if [[ ! -e ${FN_NEOVIM} ]] && [[ -e ${FN_VIM} ]]; then
 		# If needed, just copy vim-plug into neovim from vim
+        mkdir -p $(dirname ${FN_NEOVIM})
 		cp ${FN_VIM} ${FN_NEOVIM}
 	fi
+    echo "Trying to launch nvim for plugin install"
     [[ -e ${FN_NEOVIM} ]] && nvim +PlugInstall +qall
 fi
