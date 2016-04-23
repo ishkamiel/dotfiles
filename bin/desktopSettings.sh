c6929e65d40d67bffd09d370e5c8d50600a6be0b
Hans Liljestrand <ishkamiel@gmail.com>
2016-04-23 17:34:45 +0300

    let nautilus manage desktop

--- bin/desktopSettings.sh
+++ bin/desktopSettings.sh
@@ -4,7 +4,7 @@ if command -v gsettings >/dev/null 2>&1
 then
 
     # Fix desktop background and right-click
-    gsettings set org.gnome.settings-daemon.plugins.background active false
+    gsettings set org.gnome.settings-daemon.plugins.background active true
     # Disable annoying search on type thing
     gsettings set org.gnome.nautilus.preferences enable-interactive-search false
     # Enable delete contect menu action
