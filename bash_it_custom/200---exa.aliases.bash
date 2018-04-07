27e7cb5ebbe47ae826f8fa86f2190f62a340dc09
Hans Liljestrand <liljestrandh@gmail.com>
2018-04-07 13:31:14 +0300

    replace tree&ls with exa

--- bash_it_custom/200---exa.aliases.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-# exa.aliases.bash
-# Copyright (C) 2018 Hans Liljestrand <liljestrandh@gmail.com>
-#
-# Distributed under terms of the MIT license.
-#
-
-command -v exa >/dev/null 2>&1 && alias ls="exa --time-style=iso --git"
