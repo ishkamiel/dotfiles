37c093340c4bd3a17c1698aeb31b1ba64484388f
Hans Liljestrand <hans@liljestrand.dev>
2019-11-28 20:05:14 +0200

    Remove useless local config files

--- scripts/checkLocalDotfiles.sh
+++ scripts/checkLocalDotfiles.sh
@@ -9,28 +9,7 @@
 if [[ ! -e ~/.vimrc_local ]]; then
 	cat <<EOF > ~/.vimrc_local
 " Theses values are use for instance by vim-template.
-" let g:email = 'some@emailä
+" let g:email = 'some@email'
 " let g:username = 'Someone'
-EOF
-fi
-
-if [[ ! -e ~/.bashrc_local ]]; then
-	cat <<EOF > ~/.bashrc_local
-# Control whether we should use tmux locally, or at all.
-#ISHDOT_TMUX_ENABLE=1
-#ISHDOT_TMUX_ALWAYS=1
-
-# Uncomment to hide username@host when UID is for \${DEFAULT_USER}.
-#export DEFAULT_USER=${USER}
-EOF
-fi
-
-if [[ ! -e /~/.profile_local ]]; then
-	cat <<EOF > ~/.profile_local
-# Add "global" settings here, this will for login shells, and also on
-# gnome (and maybe other) logins.
-
-# Uncomment to try loading the Intel SGX SDK from the default location
-#[[ -e /opt/intel/sgxsdk/environment ]] && . /opt/intel/sgxsdk/environment
 EOF
 fi
