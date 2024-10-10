69946d0b407634a08db127cfe4f7da50e63ff8a6
Hans Liljestrand <hans@liljestrand.dev>
2024-10-10 23:58:00 +0300

    Add cargo installer and update tmux conf

--- scripts/cargo-install.sh
+++ scripts/cargo-install.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-cargo install --locked zellij
+#
+# Author: Hans Liljestrand <hans@liljestrand.dev>
+# Copyright (C) 2024 Hans Liljestrand <hans@liljestrand.dev>
+#
+# Distributed under terms of the MIT license.
+
+# TODO: Check how to install rustup
+# TODO: Update cargo
+# TODO: Add to main script
+
+rustup update
+
+cargo install --locked cargo-update
 cargo install --locked du-dust
+cargo install --locked eza
+cargo install --locked zellij
+
+cargo insta-update --locked --all
