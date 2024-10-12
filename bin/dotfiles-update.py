00621fc85201223eb0203e427233ef6fbe413c24
Hans Liljestrand <hans@liljestrand.dev>
2024-10-12 14:39:39 +0300

    Play around with dotfiles-update

---
+++ bin/dotfiles-update.py
@@ -0,0 +1,160 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Author: Hans Liljestrand <hans@liljestrand.dev>
+# Copyright (C) 2024 Hans Liljestrand <hans@liljestrand.dev>
+#
+# Distributed under terms of the MIT license.
+
+import os
+import subprocess
+import sys
+import shutil
+from pathlib import Path
+import argparse
+
+
+def say_step(step):
+    print(f"=== {step}")
+
+
+def is_missing(command):
+    return not shutil.which(command)
+
+
+class CommandRunner:
+    def __init__(self, dry_run=False):
+        self.dry_run = dry_run
+
+    def run(self, command, **kwargs):
+        command = [str(c) for c in command]
+        print(f"DRY-RUN: {' '.join(command)}")
+        if not self.dry_run:
+            return subprocess.run(command, **kwargs)
+        else:
+            return subprocess.CompletedProcess(args=command, returncode=0)
+
+    def chdir(self, path):
+        print(f"chdir {path}")
+        if not self.dry_run:
+            os.chdir(path)
+
+    def unlink(self, path):
+        print(f"DRY-RUN: unlink {path}")
+        if not self.dry_run:
+            path.unlink()
+
+
+class Main:
+    def __init__(self):
+        self.needed_cmds = ["stow", "git"]
+
+        self.dotfiles_path = Path(__file__).resolve().parent.parent
+        self.dotfiles = Path(os.getenv("DOTFILES", str(self.dotfiles_path)))
+
+        parser = argparse.ArgumentParser(description="Managing dotfiles")
+        parser.add_argument("-n", "--dry-run", action="store_true")
+        self.args = parser.parse_args()
+
+        self.runner = CommandRunner(dry_run=self.args.dry_run)
+
+    def run_script(self, script):
+        script_path = self.dotfiles / "scripts" / script
+        say_step(f"Running {script_path}")
+        self.runner.run([str(script_path)], check=True)
+
+    def check_needed_cmds(self):
+        need_to_install = [cmd for cmd in self.needed_cmds if is_missing(cmd)]
+        if need_to_install:
+            say_step(f"Need to install: {need_to_install}")
+            print(f"sudo apt-get install -y {' '.join(need_to_install)}")
+            input("Press any key to continue or Ctrl+C to abort...")
+            self.runner.run(
+                ["sudo", "apt", "install", "-y"] + need_to_install, check=True
+            )
+
+    def update_submodules(self):
+        say_step("Updating submodules")
+        self.runner.chdir(self.dotfiles)
+        self.runner.run(
+            [
+                "git",
+                "-C",
+                self.dotfiles,
+                "submodule",
+                "update",
+                "--init",
+                "--recursive",
+            ],
+            check=True,
+        )
+
+    def stow_packages(self):
+        say_step("Stowing packages")
+        stow_pkg_dir = self.dotfiles / "stow_pkgs"
+
+        for pkg in stow_pkg_dir.iterdir():
+
+            # Just try to adopt all the files
+            stow_args = [
+                "-v",
+                "--adopt",
+                "-d",
+                stow_pkg_dir,
+                "-t",
+                Path.home(),
+                pkg.name,
+            ]
+            result = self.runner.run(
+                ["stow"] + stow_args, capture_output=True, text=True
+            )
+
+            # But may need to remove old symlinks if file was managed otherwise
+            if result.returncode != 0:
+                lines = result.stderr.splitlines()
+                bad = [l.split()[-1] for l in lines if "not owned by stow:" in l]
+                for file in bad:
+                    file_path = Path.home() / file
+                    if file_path.is_symlink() and not file_path.exists():
+                        print(f"Removing dead link: {file_path}")
+                        file_path.unlink()
+
+            # Need to restore the original files since adopt may have ovrwritten them
+            self.runner.run(
+                ["git", "-C", str(self.dotfiles), "checkout", f"stow_pkgs/{pkg.name}"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+
+            # Now we can finally update the pkg symlinks
+            stow_args = ["-R", "-d", stow_pkg_dir, "-t", str(Path.home()), pkg.name]
+            result = self.runner.run(
+                ["stow"] + stow_args, capture_output=True, text=True
+            )
+            # Filter the output to avoid annoying stow BUG m message
+            # print(result.stderr.replace('BUG in find_stowed_path', ''))
+
+    def run_scripts(self):
+        scripts = [
+            "install_apt_pkgs.sh",
+            "install_cargo_pkgs.sh",
+            "install_fzf.sh",
+            "install_oh_my_zsh.sh",
+            "install_vim_plugins.sh",
+        ]
+        for script in scripts:
+            self.run_script(script)
+
+    def run(self):
+        print(f"DOTFILES: {self.dotfiles}")
+        assert self.dotfiles == self.dotfiles_path
+        self.check_needed_cmds()
+        self.update_submodules()
+        self.stow_packages()
+        # self.restow()
+        self.run_scripts()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
---
+++ lib/installers.sh
@@ -0,0 +1,103 @@
+#! /usr/bin/env bash
+#
+# Author: Hans Liljestrand <hans@liljestrand.dev>
+# Copyright (C) 2018-2024 Hans Liljestrand <hans@liljestrand.dev>
+#
+# Distributed under terms of the MIT license.
+
+[[ ${DOTFILES_LIB_INSTALLERS_SH:=0} == 1 ]] && return 0
+DOTFILES_LIB_INSTALLERS_SH=1
+
+. "${ISHLIB}/ishlib.sh"
+
+declare -A apt_install_directives=(
+  [cargo]=cargo
+  [git]=git
+  [fd]=fd-find
+  [bat]=bat
+  [eza]=eza
+)
+
+declare -A cargo_install_directives=(
+  [cargo-update]=cargo-update
+  [bat]=bat
+  [eza]=eza
+)
+
+install_apt_cmd_pkg() {
+  local cmd="$1"           # Command we need
+  local pkg="${2:-$cmd}"   # Package name that defaults to command name
+
+  if command -v apt &> /dev/null; then
+    ish_debug "Skipping, found command $cmd"
+    return 0
+  fi
+
+  if [[ -z "${apt_install_directives[$cmd]+x}" ]]; then
+    ish_debug "Cannot install $cmd with apt"
+    return 0
+  fi
+
+  ish_run -s apt-get install -y "$pkg"
+}
+
+install_cargo_cmd_pkg() {
+  local force_install=false
+
+  while getopts ":f" opt; do
+    case ${opt} in
+      f )
+        force_install=true
+        ;;
+      \? )
+        echo "Invalid option: -$OPTARG" >&2
+        return 1
+        ;;
+    esac
+  done
+  shift $((OPTIND -1))
+
+  local cmd="$1"           # Command we need
+  local pkg="${2:-$cmd}"   # Package name that defaults to command name
+
+  # Check if we have $cmd available, unless -f was used to force install
+  if [[ "$force_install" != true ]]; then
+    # Make sure cargo is in the PATH so we can actually check
+    ish_prepend_to_path "${HOME}/.cargo/bin"
+
+    if command -v "$cmd" &> /dev/null; then
+      # And we're done since the command was already found
+      ish_debug "Skipping install, found command $cmd"
+      return 0
+    fi
+  fi
+
+  ish_run cargo install --locked "$pkg"
+}
+
+install_cmd_somehow() {
+  local cmd="$1"           # Command we need
+  local pkg="${2:-$cmd}"   # Package name that defaults to command name
+
+  if command -v "$cmd" &> /dev/null; then
+    ish_debug "Skipping install, found command $cmd"
+    return 0
+  fi
+
+  # Try to install with cargo
+  if [[ -n "${cargo_install_directives[$cmd]+x}" ]]; then
+    if install_cargo_cmd_pkg "$cmd" "${cargo_install_directives[$cmd]}"; then
+      return 0
+    fi
+  fi
+
+  # Try to install with apt
+  if [[ -n "${apt_install_directives[$cmd]+x}" ]]; then
+    if install_apt_cmd_pkg "$cmd" "${apt_install_directives[$cmd]}"; then
+      return 0
+    fi
+  fi
+
+  ish_warn "No install directive for $cmd"
+  return 1
+}
