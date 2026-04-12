5cda8458104a0b35636e04ec4bc2c29fd142fb3c
Hans Liljestrand <hans@liljestrand.dev>
2026-04-12 15:05:58 +0300

    Update dev stuff

--- run_pytest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# SPDX-License-Identifier: MIT
-# Copyright (C) 2026 Hans Liljestrand <hans@liljestrand.dev>
-
-eval "$(direnv hook bash)"
-pytest="$(which pytest)"
-exec direnv exec "$pytest" "$@"
